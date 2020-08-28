242415a64912cb09aa5bd14f322264e73b0b1097
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-08-28 19:44:25 +0300

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {useStyles} from '../styles/styles'
 import {useStoreActions} from 'easy-peasy'
 import {CssBaseline} from '@material-ui/core'
-import TopBar from './topbar'
 import MapPage from './Map/map-page'
 
 const App = () => {
@@ -13,7 +12,7 @@ const App = () => {
         <CssBaseline>
             <div className={classes.root}>
                 <main className={classes.content}>
-                    <TopBar classes={classes}/><MapPage/>
+                    <MapPage/>
                 </main>
             </div>
         </CssBaseline>
