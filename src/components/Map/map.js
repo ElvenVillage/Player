77e8a67765c2db2cd91c6267d182c72c784d0467
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-08-28 19:44:28 +0300

    Update map.js

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react'
-import {Map, Marker, TileLayer, Polyline, ScaleControl} from 'react-leaflet'
+import {Map, Marker, TileLayer, Polyline, ScaleControl, Popup} from 'react-leaflet'
 import {useStoreState, useStoreActions} from 'easy-peasy'
 import {useInterval, calculateAngle} from '../../misc/handlers'
 import Wind from './wind'
@@ -8,17 +8,19 @@ import Player from './player'
 import {CustomMarker, PinIcon} from '../../misc/graphics';
 
 export const MapContainer = ({center, setCenter}) => {
-    const {endTime, currentTime} = useStoreState(state => state.player);
-    const {classes} = useStoreState(state => state.classes);
-    const {boats, startLine, isPlacing} = useStoreState(state => state.boats);
-    const {setCurrentTime} = useStoreActions(actions => actions.player);
-    const {setCurrentBoatsSpeed, setStartLine, updateStartLine} = useStoreActions(actions => actions.boats);
-    const [visible, setVisible] = useState(false);
-    const [delay, setDelay] = useState(1000);
-    const [isPlaying, setPlaying] = useState(false);
-    const [wind, setWind] = useState(-1);
-    const [avgWind, setAvgWind] = useState(-1);
-    const mapRef = useRef(null);
+    const {endTime, currentTime} = useStoreState(state => state.player)
+    const {classes} = useStoreState(state => state.classes)
+    const {boats, startLine, isPlacing} = useStoreState(state => state.boats)
+    const {setCurrentTime} = useStoreActions(actions => actions.player)
+    const {setCurrentBoatsSpeed, setStartLine, updateStartLine} = useStoreActions(actions => actions.boats)
+    const [visible, setVisible] = useState(false)
+    const [delay, setDelay] = useState(1000)
+    const [isPlaying, setPlaying] = useState(false)
+    const [wind, setWind] = useState(0)
+
+    const {isReady} = useStoreState(state => state.player)
+    const {setIsReady} = useStoreActions(actions => actions.player)
+    const mapRef = useRef(null)
 
     const [ListOfMarkers, setListOfMarkers] = useState([])
 
@@ -30,144 +32,141 @@ export const MapContainer = ({center, setCenter}) => {
 
     useEffect(() => {
         if (boats.length > 0) {
-            setCenter(boats[0].center);
-            updateWindDirection();
-            setAvgWindDirection();
+            setCenter(boats[0].center)
+            updateWindDirection()
         }
-        updateDivs();
-    }, []);
+        updateDivs()
+    }, [])
 
     useEffect(() => {
-        updateDivs();
+        updateDivs()
         window.addEventListener('resize', updateDivs)
     });
 
     const updateWindDirection = () => {
-        let windDirection = 0;
+        let windDirection = 0
+        let wasCouchViewed = false
+
         boats.forEach(boat => {
             if (boat.data.length > currentTime) {
-                windDirection += boat.data[currentTime].WIND_DIR;
+                if ((boat.data[currentTime].AWA != -1) && (!wasCouchViewed)) {
+                    windDirection += boat.data[currentTime].AWA
+                    wasCouchViewed = true
+                    setIsReady(true)
+                }
             } else {
-                windDirection += boat.data[boat.data.length - 1].WIND_DIR;
+                if ((boat.data[boat.data.length - 1].AWA != -1) && (!wasCouchViewed)) {
+                    windDirection += boat.data[boat.data.length - 1].AWA
+                    wasCouchViewed = true
+                    setIsReady(true)
+                }
             }
-        });
-        windDirection /= boats.length;
-        setWind(windDirection);
-        const wind = document.getElementById('wind-div');
-        updateDivWidthHeight([wind].filter(val => val !== null));
+        })
+        setWind(windDirection)
+        const wind = document.getElementById('wind-div')
+        updateDivWidthHeight([wind].filter(val => val !== null))
     }
 
-    const setAvgWindDirection = () => {
-        let wind = 0;
-        if (boats.length > 0) {
-            boats.forEach(boat => {
-                boat.data.forEach(obj => wind += obj.WIND_DIR);
-                wind /= boat.data.length;
-            });
-            wind /= boats.length;
-        }
-        setAvgWind(wind);
-    }
 
     const updateDivWidthHeight = (elements) => {
-        if (!elements || elements.length === 0) return;
-        const mapContainer = document.getElementById('leaflet-map');
-        const style = window.getComputedStyle(mapContainer);
+        if (!elements || elements.length === 0) return
+        const mapContainer = document.getElementById('leaflet-map')
+        const style = window.getComputedStyle(mapContainer)
         elements.forEach(element => {
             if (element.id === "player-div") {
-                const width = parseFloat(style.width, 10);
-                const height = parseFloat(style.height, 10);
-                element.style.width = 0.9 * width + 'px';
-                element.style.height = 0.15 * height + 'px';
-                element.style.margin = height * 0.15 + 'px ' + 0.05 * width + 'px';
-                return;
+                const width = parseFloat(style.width, 10)
+                const height = parseFloat(style.height, 10)
+                element.style.width = 0.9 * width + 'px'
+                element.style.height = 0.15 * height + 'px'
+                element.style.margin = height * 0.15 + 'px ' + 0.05 * width + 'px'
+                return
             }
             if (element.id === 'wind-div') {
-                const width = parseFloat(style.width, 10);
-                const height = parseFloat(style.height, 10);
+                const width = parseFloat(style.width, 10)
+                const height = parseFloat(style.height, 10)
                 //const margin = (height - width) / 2;
-                element.style.width = width + 'px';
-                element.style.height = 0.1 * height + 'px';
+                element.style.width = width + 'px'
+                element.style.height = 0.1 * height + 'px'
                 //element.style.margin = margin + 'px ' + 'auto';
-                return;
+                return
             } else if (element.id === "north-div") {
-                const height = parseFloat(style.height, 10);
-                element.style.width = style.width;
-                element.style.height = 0.1 * height + 'px';
-                return;
+                const height = parseFloat(style.height, 10)
+                element.style.width = style.width
+                element.style.height = 0.1 * height + 'px'
+                return
             }
-            element.style.width = style.width;
-            element.style.height = style.height;
+            element.style.width = style.width
+            element.style.height = style.height
         })
     }
 
     const updateBoatsAngle = () => {
-        const boatDivs = document.getElementsByClassName('boat-div');
-        if (!boatDivs || boatDivs.length === 0) return;
+        const boatDivs = document.getElementsByClassName('boat-div')
+        if (!boatDivs || boatDivs.length === 0) return
         boats.forEach((boat, index) => {
-            const [svgChild] = [...boatDivs[index].childNodes].filter(child => child.nodeName === 'svg');
-            let pos1, pos2 = [];
+            const [svgChild] = [...boatDivs[index].childNodes].filter(child => child.nodeName === 'svg')
+            let pos1, pos2 = []
             if (currentTime === 0) {
-                pos1 = pos2 = boat.coords[0];
+                pos1 = pos2 = boat.coords[0]
             } else {
-                let coordsLen = boat.coords.length - 1;
-                pos1 = currentTime > coordsLen ? boat.coords[coordsLen] : boat.coords[currentTime - 1];
-                pos2 = currentTime > coordsLen ? boat.coords[coordsLen] : boat.coords[currentTime];
+                let coordsLen = boat.coords.length - 1
+                pos1 = currentTime > coordsLen ? boat.coords[coordsLen] : boat.coords[currentTime - 1]
+                pos2 = currentTime > coordsLen ? boat.coords[coordsLen] : boat.coords[currentTime]
             }
-            let angle = calculateAngle(pos1, pos2);
-            svgChild.style.transform = `rotate(${angle}deg)`;
-            svgChild.childNodes[3].style.fill = boat.color;
+            let angle = calculateAngle(pos1, pos2)
+            svgChild.style.transform = `rotate(${angle}deg)`
+            svgChild.childNodes[3].style.fill = boat.color
         });
     }
 
     const updateDivs = () => {
-        const player = document.getElementById('player-div');
-        const north = document.getElementById('north-div');
-        const wind = document.getElementById('wind-div');
-        const elements = [player, north, wind].filter(val => val !== null);
-        updateDivWidthHeight(elements);
-        updateBoatsAngle();
+        const player = document.getElementById('player-div')
+        const north = document.getElementById('north-div')
+        const wind = document.getElementById('wind-div')
+        const elements = [player, north, wind].filter(val => val !== null)
+        updateDivWidthHeight(elements)
+        updateBoatsAngle()
     }
 
     const updateSpeed = (val = currentTime) => {
-        const speeds = [];
+        const speeds = []
         boats.forEach(boat => {
-            const curData = boat.data;
+            const curData = boat.data
             if (curData.length - 1 > val) {
-                const speed = Math.round(curData[val].SPD * 100) / 100;
-                speeds.push(speed);
+                const speed = Math.round(curData[val].SPD * 100) / 100
+                speeds.push(speed)
             } else {
-                const speed = Math.round(curData[curData.length - 1].SPD * 100) / 100;
-                speeds.push(speed);
+                const speed = Math.round(curData[curData.length - 1].SPD * 100) / 100
+                speeds.push(speed)
             }
-        });
-        setCurrentBoatsSpeed(speeds);
+        })
+        setCurrentBoatsSpeed(speeds)
     }
 
     const handleUpdateTime = (e, val) => {
-        setCurrentTime(val);
-        updateSpeed(val);
-        updateWindDirection();
+        setCurrentTime(val)
+        updateSpeed(val)
+        updateWindDirection()
     }
 
     useInterval(() => {
         if (currentTime >= endTime - 1) {
-            setCurrentTime(endTime);
-            setPlaying(false);
+            setCurrentTime(endTime)
+            setPlaying(false)
         }
-        setCurrentTime(currentTime + 1);
-        updateSpeed();
-        updateWindDirection();
-    }, isPlaying ? delay : null);
+        setCurrentTime(currentTime + 1)
+        updateSpeed()
+        updateWindDirection()
+    }, isPlaying ? delay : null)
 
     const handleClick = (e) => {
-        const map = mapRef.current;
+        const map = mapRef.current
         if (!isPlacing || !map) {
-            return;
+            return
         }
-        const {latlng} = e;
-        const clickedCoords = [latlng.lat, latlng.lng];
-        setStartLine(clickedCoords);
+        const {latlng} = e
+        const clickedCoords = [latlng.lat, latlng.lng]
+        setStartLine(clickedCoords)
     }
 
     useEffect(() => {
@@ -203,20 +202,20 @@ export const MapContainer = ({center, setCenter}) => {
 
     const OnCreateSetBuiTitle = (id) => {
         if (id == 0) {
-            return ('port');
+            return ('port')
         }
         if (id == 1) {
-            return ('starboard');
+            return ('starboard')
         } else {
             return ('Marker' + (id-1))
         }
     }
     ////////
     const handleDragEnd = (e) => {
-        const {id} = e.target.options;
-        const latlng = e.target.getLatLng();
-        const coords = [latlng.lat, latlng.lng];
-        updateStartLine({id, coords});
+        const {id} = e.target.options
+        const latlng = e.target.getLatLng()
+        const coords = [latlng.lat, latlng.lng]
+        updateStartLine({id, coords})
     }
 
     const markers = ListOfMarkers.length < 1 && ListOfMarkers !== null
@@ -230,9 +229,9 @@ export const MapContainer = ({center, setCenter}) => {
                         position={marker}
                         draggable={true}
                         key={`marker-${idx}`}
-                        icon={PinIcon}
-                        title={OnCreateSetBuiTitle(idx)}
-                    />
+                        icon={PinIcon} >
+                        <Popup>{OnCreateSetBuiTitle(idx)}</Popup>
+                    </Marker>
                 ))}
                 <Polyline className={classes.polyline} color={'black'} positions={startLine}/>
             </>
@@ -244,7 +243,7 @@ export const MapContainer = ({center, setCenter}) => {
             onMouseLeave={isPlacing ? null : () => handleShowPlayer(false)}
         >
             <North/>
-            <Wind wind={wind} avgWind={avgWind}/>
+            <Wind wind={wind} isReady={isReady}/>
             <Map
                 id="leaflet-map"
                 center={center}
